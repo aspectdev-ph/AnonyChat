e09be7eed30029103e2b64905c7350ede3e1119d
aspectdev-ph <ajdapulang@gmail.com>
2021-03-15 09:34:51 +0800

    Update app.php

--- app.php
+++ app.php
@@ -5,7 +5,7 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Socket;
 
-require dirname( __FILE__ ) . '/MyApp/Socket.php';
+require '/MyApp/Socket.php';
 require dirname( __FILE__ ) . '/vendor/autoload.php';
 
 $server = IoServer::factory(
