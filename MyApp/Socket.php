93f58fb0382502ae499e80fc1b9c15312cd88328
aspectdev-ph <ajdapulang@gmail.com>
2021-03-15 09:32:01 +0800

    App

--- MyApp/Socket.php
+++ MyApp/Socket.php
@@ -5,7 +5,6 @@ namespace MyApp;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
-require dirname(__FILE__) . '/MyApp/socket.php';
 require dirname(__FILE__) . '/vendor/autoload.php';
 class Socket implements MessageComponentInterface
 {
